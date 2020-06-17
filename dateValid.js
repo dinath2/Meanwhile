1fc930d94157a07bfab14e4f41472637d23e80e6
dinath2 <65164585+dinath2@users.noreply.github.com>
2020-06-17 12:58:37 +0200

    TAKK SINDRE

--- dateValid.js
+++ dateValid.js
@@ -1,11 +1,17 @@
 function isDateValid(date){
-    //ex: 03.02.2020
-const year = date.substring(6,9);
-const month = date.substring(4,5);
-const day = date.substring (0,2);
+    // ex: 03.02.2020
+    const year = date.substring(6,10);
+    const month = date.substring(3,5);
+    const day = date.substring (0,2);
 
-    //insert if
-    if (dateLength(date) && dateFormat(date) && monthLength(date) && validYear(year) && isLeapYear(year)) 
+    if (
+        dateLength(date)
+        && dateFormat(date)
+        && dayLength(day)
+        && monthLength(month)
+        && validYear(year)
+        && (february(day,month,year) || evenMonth(day, month) || oddMonth(day, month))
+        )
     {return true;} else {return false;}
 }
 
@@ -17,28 +23,41 @@ function dateFormat(date) {
     return date.indexOf(".") == 2 && date.lastIndexOf(".") == 5;
 }
 
-function monthLenght(date) {
-    return month.length == 2 
+function monthLength(month) {
+    return month.length === 2 
     && month >= 1 
     && month <= 12;
 }
 
-function dayLength(date) {
-    return day.length == 2
-    && ;
+function dayLength(day) {
+    return day.length === 2;
 }
 
-function evenMonth(date) {
-
+function february(day,month,year) {
+    return (month == 2
+    && day >= 1
+    && day <=28) || (
+    month == 2
+    && day >= 1
+    && day <=29
+    && isLeapYear(year));
 }
 
-function oddMonth(date) {
-    
+function evenMonth(day, month) {
+    return month == (4 || 6 || 8 || 10 || 12)
+    && day >= 1
+    && day <= 30;
+}
+
+function oddMonth(day, month) {
+    return month == (1 || 3 || 5 || 7 || 9 || 11)
+    && day >= 1
+    && day <= 31;
 }
 
 function validYear(year) {
-    return year.length === 4 
-    && year >= '0000' 
+    return year.length === 4
+    && year >= '0000'
     && year <= '9999';
 }
 
